7c2f3d66b8d58af806a3d35eeed1d2a4bad2e369
Ruben Labruyere <info@rubenlabruyere.be>
2021-04-03 14:09:45 +0200

    Fixed a nullref exception

--- src/WebServer.cs
+++ src/WebServer.cs
@@ -36,6 +36,7 @@ namespace Jarmer.WebServer
             this.host = Dns.GetHostName();
             this.port = port;
         }
+
         public WebServer(string host, int port)
         {
             this.host = host;
@@ -73,10 +74,12 @@ namespace Jarmer.WebServer
                     {
                         throw new Exception($"Method {methodInfo.Name} in controller {controllerType.Name} has a {httpAttrib.GetType().Name} attribute defined but returns no ActionResult object");
                     }
+
                     if (ReturnsActionResult(methodInfo) && httpAttrib == null)
                     {
                         throw new Exception($"Method {methodInfo.Name} in controller {controllerType.Name} returns an ActionResult object but has no Http attribute defined");
                     }
+
                     if (!methodInfo.IsPublic && httpAttrib != null)
                     {
                         throw new Exception($"Action {methodInfo.Name} in controller {controllerType.Name} is not public");
@@ -111,14 +114,17 @@ namespace Jarmer.WebServer
                 HandleResult(response, status, new TextResult(error));
             }
         }
+
         private void Server_OnRequestStart(HttpRequest request, HttpConnectionInfo connectionInfo)
         {
             OnRequestStart?.Invoke(request, connectionInfo);
         }
+
         private void Server_OnRequestEnd(HttpRequest request, HttpResponse response, HttpConnectionInfo connectionInfo)
         {
             OnRequestEnd?.Invoke(request, response, connectionInfo);
         }
+
         private void Server_OnRequest(HttpRequest request, HttpResponse response, HttpConnectionInfo info)
         {
             try
@@ -170,10 +176,10 @@ namespace Jarmer.WebServer
                     foreach (var attrib in attributes)
                     {
                         Type attribType = attrib.GetType();
-                        
+
                         if (attribType.BaseType == typeof(CustomWebAttribute))
                         {
-                            var customAttrib = (CustomWebAttribute)attrib;
+                            var customAttrib = (CustomWebAttribute) attrib;
                             var result = customAttrib.Handle(request);
 
                             if (result != null)
@@ -191,9 +197,10 @@ namespace Jarmer.WebServer
 
             throw new HttpException(HttpStatusCode.NotFound, $"Action or content {request.Method.ToString().ToUpper()} {request.Path} not found");
         }
+
         private void HandleAction(HttpRequest request, HttpConnectionInfo info, HttpResponse response, Type controllerType, MethodInfo methodInfo)
         {
-            var controller = (IController)Activator.CreateInstance(controllerType);
+            var controller = (IController) Activator.CreateInstance(controllerType);
             var arguments = GenerateActionArgs(request, methodInfo);
 
             // Set required parameters
@@ -212,7 +219,7 @@ namespace Jarmer.WebServer
                 }
                 else
                 {
-                    var actionResult = (ActionResult)result;
+                    var actionResult = (ActionResult) result;
 
                     // Set cookies and session variables
                     foreach (var entry in controller.Cookies.ToList())
@@ -229,6 +236,7 @@ namespace Jarmer.WebServer
                 ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
             }
         }
+
         private object[] GenerateActionArgs(HttpRequest request, MethodInfo methodInfo)
         {
             var contentTypeAttrib = methodInfo.GetCustomAttribute<ContentTypeAttribute>();
@@ -243,7 +251,7 @@ namespace Jarmer.WebServer
             }
 
             // Update args with defaults
-            for (var i=0; i<args.Length; i++)
+            for (var i = 0; i < args.Length; i++)
             {
                 if (parameters[i].HasDefaultValue)
                 {
@@ -313,7 +321,7 @@ namespace Jarmer.WebServer
                 // In that case, the argument will be null
                 if (parameters.Length > 1)
                 {
-                    for (var i=0; i<parameters.Length; i++)
+                    for (var i = 0; i < parameters.Length; i++)
                     {
                         var param = parameters[i];
                         var attrib = param.GetCustomAttribute<FromBodyAttribute>();
@@ -328,6 +336,7 @@ namespace Jarmer.WebServer
                             args[i] = request.CharSet.GetString(request.Body.Data);
                             continue;
                         }
+
                         if (param.ParameterType == typeof(byte[]))
                         {
                             args[i] = request.Body.Data;
@@ -356,57 +365,62 @@ namespace Jarmer.WebServer
                 }
 
                 // Then match all remaining fields
-                for (var i=0; i<parameters.Length; i++)
+                if (request.Body.Files != null)
                 {
-                    var parameter = parameters[i];
-                    var attrib = parameter.GetCustomAttribute<FromBodyAttribute>();
-                    var file = request.Body.Files.FirstOrDefault(x => x.Key == parameter.Name);
+                    for (var i = 0; i < parameters.Length; i++)
+                    {
+                        var parameter = parameters[i];
+                        var attrib = parameter.GetCustomAttribute<FromBodyAttribute>();
+                        var file = request.Body.Files.FirstOrDefault(x => x.Key == parameter.Name);
 
-                    // Continue only when a match was found
-                    // Or when there is only 1 parameter
-                    if (parameters.Length == 1)
-                    {
-                        file = request.Body.Files.FirstOrDefault();
-                    }
-                    if (file == null)
-                    {
-                        continue;
-                    }
-
-                    // The webserver will convert the file or its contents depending on the parameter type
-                    if (parameter.ParameterType == typeof(HttpFile))
-                    {
-                        args[i] = file;
-                    }
-                    if (parameter.ParameterType == typeof(byte[]))
-                    {
-                        args[i] = file.Data;
-                    }
-
-                    // The webserver also supports an array of httpfiles, which is exactly the same type as the request.Body.Files property
-                    // In this case we just assing the argument without conversion
-                    if (parameter.ParameterType == typeof(HttpFile[]))
-                    {
-                        args[i] = request.Body.Files;
-                    }
-
-                    // String conversion is a special case because we don't know the encoding unless it was set as part of the content type
-                    // Therefore we will assume the encoding was default unless specified otherwise
-                    if (parameter.ParameterType == typeof(string))
-                    {
-                        args[i] = file.CharSet.GetString(file.Data);
-                    }
-
-                    // The webserver will also convert json if the content type is json and the frombody attrib is set
-                    if (attrib != null && file.ContentType.StartsWith("application/json"))
-                    {
-                        try
+                        // Continue only when a match was found
+                        // Or when there is only 1 parameter
+                        if (parameters.Length == 1)
                         {
-                            args[i] = JsonConvert.DeserializeObject(file.CharSet.GetString(file.Data), parameters[i].ParameterType);
+                            file = request.Body.Files.FirstOrDefault();
                         }
-                        catch (JsonReaderException ex)
+
+                        if (file == null)
                         {
-                            throw new HttpException(HttpStatusCode.BadRequest, ex.Message);
+                            continue;
+                        }
+
+                        // The webserver will convert the file or its contents depending on the parameter type
+                        if (parameter.ParameterType == typeof(HttpFile))
+                        {
+                            args[i] = file;
+                        }
+
+                        if (parameter.ParameterType == typeof(byte[]))
+                        {
+                            args[i] = file.Data;
+                        }
+
+                        // The webserver also supports an array of httpfiles, which is exactly the same type as the request.Body.Files property
+                        // In this case we just assing the argument without conversion
+                        if (parameter.ParameterType == typeof(HttpFile[]))
+                        {
+                            args[i] = request.Body.Files;
+                        }
+
+                        // String conversion is a special case because we don't know the encoding unless it was set as part of the content type
+                        // Therefore we will assume the encoding was default unless specified otherwise
+                        if (parameter.ParameterType == typeof(string))
+                        {
+                            args[i] = file.CharSet.GetString(file.Data);
+                        }
+
+                        // The webserver will also convert json if the content type is json and the frombody attrib is set
+                        if (attrib != null && file.ContentType.StartsWith("application/json"))
+                        {
+                            try
+                            {
+                                args[i] = JsonConvert.DeserializeObject(file.CharSet.GetString(file.Data), parameters[i].ParameterType);
+                            }
+                            catch (JsonReaderException ex)
+                            {
+                                throw new HttpException(HttpStatusCode.BadRequest, ex.Message);
+                            }
                         }
                     }
                 }
@@ -418,7 +432,7 @@ namespace Jarmer.WebServer
                 // If the parameter type holds any other value we are just going to ignore it and leave it up to the user to convert it
                 if (request.Body != null)
                 {
-                    for (var i=0; i<parameters.Length; i++)
+                    for (var i = 0; i < parameters.Length; i++)
                     {
                         var parameter = parameters[i];
 
@@ -426,6 +440,7 @@ namespace Jarmer.WebServer
                         {
                             args[i] = request.Body.Data;
                         }
+
                         if (parameter.ParameterType == typeof(string))
                         {
                             args[i] = request.CharSet.GetString(request.Body.Data);
@@ -436,9 +451,10 @@ namespace Jarmer.WebServer
 
             return args;
         }
+
         private void UpdateArgsWithQuery(Dictionary<string, string> query, ParameterInfo[] parameters, ref object[] args)
         {
-            for (var i=0; i<parameters.Length; i++)
+            for (var i = 0; i < parameters.Length; i++)
             {
                 var paramInfo = parameters[i];
                 var attrib = paramInfo.GetCustomAttribute<FromBodyAttribute>();
@@ -450,7 +466,7 @@ namespace Jarmer.WebServer
                     continue;
                 }
 
-                for (var j=0; j<query.Count; j++)
+                for (var j = 0; j < query.Count; j++)
                 {
                     var key = query.Keys.ToArray()[j].ToLower();
                     var value = query.Values.ToArray()[j];
@@ -462,33 +478,34 @@ namespace Jarmer.WebServer
                 }
             }
         }
+
         private void HandleResult(HttpResponse response, HttpStatusCode statusCode, ActionResult result)
         {
             response.StatusCode = statusCode;
             response.Headers.Server = "Reapenshaw Web Server";
-            
+
             if (result.GetType() == typeof(JsonResult))
             {
-                var jsonResult = (JsonResult)result;
+                var jsonResult = (JsonResult) result;
                 var json = JsonConvert.SerializeObject(jsonResult.Body);
 
                 response.Headers.ContentType = "application/json";
                 response.Body = Encoding.ASCII.GetBytes(json);
                 response.Send();
             }
-            
+
             if (result.GetType() == typeof(TextResult))
             {
-                var textResult = (TextResult)result;
+                var textResult = (TextResult) result;
 
                 response.Headers.ContentType = "text/plain; charset=utf-8";
                 response.Body = Encoding.ASCII.GetBytes(textResult.Text);
                 response.Send();
             }
-            
+
             if (result.GetType() == typeof(RedirectResult))
             {
-                var redirectResult = (RedirectResult)result;
+                var redirectResult = (RedirectResult) result;
 
                 response.Headers.Location = redirectResult.Url;
                 response.StatusCode = HttpStatusCode.Redirect;
@@ -509,7 +526,7 @@ namespace Jarmer.WebServer
             {
                 var contentResult = (ContentResult) result;
                 var contentPath = ContentPath(contentResult.Path);
-                
+
                 response.SendFile(contentPath);
             }
         }
@@ -518,19 +535,22 @@ namespace Jarmer.WebServer
         {
             var wwwPath = $"www{path}";
 
-            if (wwwPath.EndsWith("/")) {
+            if (wwwPath.EndsWith("/"))
+            {
                 wwwPath += "index.html";
             }
 
             return wwwPath;
         }
+
         private bool ReturnsActionResult(MethodInfo methodInfo)
         {
             var returnType = methodInfo.ReturnType;
             var returnBaseType = methodInfo.ReturnType != null ? methodInfo.ReturnType.BaseType : null;
-            
+
             return returnBaseType == typeof(ActionResult) || returnType == typeof(ActionResult);
         }
+
         private Type[] GetControllerTypes()
         {
             var assembly = Assembly.GetEntryAssembly();
@@ -539,6 +559,7 @@ namespace Jarmer.WebServer
 
             return controllerTypes.ToArray();
         }
+
         private MethodInfo[] GetControllerActions(Type controllerType)
         {
             var methods = controllerType.GetMethods();
@@ -547,11 +568,12 @@ namespace Jarmer.WebServer
             foreach (var method in methods)
             {
                 var httpAttrib = method.GetCustomAttribute<HttpAttribute>();
-                
+
                 if (httpAttrib == null)
                 {
                     continue;
                 }
+
                 if (!ReturnsActionResult(method))
                 {
                     continue;
@@ -567,11 +589,11 @@ namespace Jarmer.WebServer
         {
             var output = input;
 
-            for (var j=255; j>0; j--)
+            for (var j = 255; j > 0; j--)
             {
                 var hex = j.ToString("X");
                 var src = $"%{hex}";
-                var dest = ((char)j).ToString();
+                var dest = ((char) j).ToString();
 
                 output = output.Replace(src, dest);
             }
@@ -581,8 +603,11 @@ namespace Jarmer.WebServer
 
         /* EVENTS */
         public delegate ActionResult OnHttpErrorDelegate(HttpRequest request, HttpStatusCode status, string error);
+
         public delegate void OnExceptionDelegate(Exception exception);
+
         public delegate void OnRequestStartDelegate(HttpRequest request, HttpConnectionInfo connectionInfo);
+
         public delegate void OnRequestEndDelegate(HttpRequest request, HttpResponse response, HttpConnectionInfo connectionInfo);
 
         public event OnHttpErrorDelegate OnHttpError;
