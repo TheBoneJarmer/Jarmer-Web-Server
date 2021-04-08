06971cc985da2bb096832180a97bb75a1a3dac12
Ruben Labruyere <info@rubenlabruyere.be>
2021-04-08 19:24:59 +0200

    Updated script and csproj

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
 ﻿$branch = git branch --show-current;
 
 Remove-Item *.nupkg
-dotnet pack -c Release -o . ./src/Jarmer.WebServer.csproj
+dotnet pack -c Release -o . ./src/WebserverCS.csproj
 
 if ($branch -eq "develop")
 {
